22f638140164d96d3691b5f584b5ae783000766b
Saurabh Maurya <saurabhmaurya27012004@gmail.com>
2024-12-29 23:22:51 +0530

    frontend updated now

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import {BrowserRouter,Routes,Route, Links} from "react-router-dom";
+import Home from "./components/Home.jsx";
+import Videos from './components/Videos.jsx';
+import Tweets from './components/Tweets.jsx';
+import Documents from './components/Documents.jsx';
+import Links from './components/Links.jsx';
+import Tags from './components/Tags.jsx';
+import Signin from './components/Signin.jsx';
+import Signup from './components/Signup.jsx';
+
 
 const App = () => {
   return (
-    <div> 
-        <h3 className='text-xl'>Brainly <span className='text-red-400'>App</span></h3>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/tweets" element={<Tweets/>}/>
+          <Route path="/videos" element={<Videos/>}/>
+          <Route path="/documents" element={<Documents/>}/>
+          <Route path="/links" element={<Links/>}/>
+          <Route path="/tags" element={<Tags/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/signin" element={<Signin/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
