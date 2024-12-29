dcdfbffda371b7ba6c3783092232aa3151397333
Saurabh Maurya <saurabhmaurya27012004@gmail.com>
2024-12-29 23:48:37 +0530

    Signup and Signin Page Update !

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,12 @@ import Home from "./components/Home.jsx";
 import Videos from './components/Videos.jsx';
 import Tweets from './components/Tweets.jsx';
 import Documents from './components/Documents.jsx';
-import Links from './components/Links.jsx';
+// import Links from './components/LinksPage.jsx';
 import Tags from './components/Tags.jsx';
 import Signin from './components/Signin.jsx';
 import Signup from './components/Signup.jsx';
+import LinksPage from './components/LinksPage.jsx';
+import { ToastContainer } from 'react-toastify';
 
 
 const App = () => {
@@ -19,12 +21,13 @@ const App = () => {
           <Route path="/tweets" element={<Tweets/>}/>
           <Route path="/videos" element={<Videos/>}/>
           <Route path="/documents" element={<Documents/>}/>
-          <Route path="/links" element={<Links/>}/>
+          <Route path="/links" element={<LinksPage/>}/>
           <Route path="/tags" element={<Tags/>}/>
           <Route path="/signup" element={<Signup/>}/>
           <Route path="/signin" element={<Signin/>}/>
         </Routes>
       </BrowserRouter>
+      <ToastContainer/>
     </>
   )
 }
